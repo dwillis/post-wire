1610f74f12aa920867d121a16f3274fdcf4d1083
Derek Willis <dwillis@gmail.com>
2012-04-14 23:05:08 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Postwire::Application.config.secret_token = 'c4423fd83510bfae7a2e64650a62b63c804c13175dda29b6d5998968e64ca6ffbb2edf324cc96d7a2374bc7bb8de9288a4d7d9833a65a2f265c681172170a48d'
