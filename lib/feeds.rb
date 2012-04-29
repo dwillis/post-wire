d4cab7531c97d06ec8c4c61a578df4837a22fbef
Derek Willis <dwillis@gmail.com>
2012-04-29 13:27:11 -0400

    added feeds lib

---
+++ lib/feeds.rb
@@ -0,0 +1,61 @@
+class Feeds
+  
+  MAIN = [
+     'http://feeds.washingtonpost.com/rss/rss_the-fix',
+     'http://feeds.washingtonpost.com/rss/rss_ezra-klein'
+    ]
+  
+  POLITICS = [
+    'http://feeds.washingtonpost.com/rss/politics',
+    'http://feeds.washingtonpost.com/rss/politics/campaigns',
+    'http://feeds.washingtonpost.com/rss/politics/congress',
+    'http://feeds.washingtonpost.com/rss/politics/courts-law',
+    'http://feeds.washingtonpost.com/rss/politics/federal-government',
+    'http://feeds.washingtonpost.com/rss/politics/polling',
+    'http://feeds.washingtonpost.com/rss/politics/whitehouse',
+    'http://feeds.washingtonpost.com/rss/politics/blogs-columns',
+    'http://feeds.washingtonpost.com/rss/rss_2chambers',
+    'http://feeds.washingtonpost.com/rss/rss_44',
+    'http://feeds.washingtonpost.com/rss/rss_behind-the-numbers',
+    'http://feeds.washingtonpost.com/rss/rss_election-2012',
+    'http://feeds.washingtonpost.com/rss/rss_fact-checker',
+    'http://feeds.washingtonpost.com/rss/rss_federal-eye',
+    'http://feeds.washingtonpost.com/rss/rss_in-the-loop',
+    'http://feeds.washingtonpost.com/rss/rss_political-bookworm',
+    'http://feeds.washingtonpost.com/rss/rss_she-the-people',
+    'http://feeds.washingtonpost.com/rss/rss_the-fix',
+    'http://feeds.washingtonpost.com/rss/rss_think-tanked'
+    ]
+    
+  OPINIONS = [
+    'http://feeds.washingtonpost.com/rss/opinions',
+    'http://feeds.washingtonpost.com/rss/opinions/left-leaning',
+    'http://feeds.washingtonpost.com/rss/opinions/right-leaning',
+    'http://feeds.washingtonpost.com/rss/opinions/local-opinions',
+    'http://feeds.washingtonpost.com/rss/opinions/personalities',
+    'http://feeds.washingtonpost.com/rss/rss_achenblog',
+    'http://feeds.washingtonpost.com/rss/rss_compost',
+    'http://feeds.washingtonpost.com/rss/rss_erik-wemple',
+    'http://feeds.washingtonpost.com/rss/rss_omblog',
+    'http://feeds.washingtonpost.com/rss/rss_plum-line',
+    'http://feeds.washingtonpost.com/rss/rss_post-partisan',
+    'http://feeds.washingtonpost.com/rss/rss_right-turn',
+    'http://feeds.washingtonpost.com/rss/rss_the-insiders'
+    ]
+    
+  LOCAL = [
+    'http://feeds.washingtonpost.com/rss/local',
+    'http://feeds.washingtonpost.com/rss/local/crime',
+    'http://feeds.washingtonpost.com/rss/local/education',
+    'http://feeds.washingtonpost.com/rss/local/obituaries',
+    'http://feeds.washingtonpost.com/rss/local/traffic-commuting',
+    'http://feeds.washingtonpost.com/rss/local/blogs-columns',
+    'http://feeds.washingtonpost.com/rss/local/therootdc',
+    'http://feeds.washingtonpost.com/rss/rss_all-opinions-are-local',
+    'http://feeds.washingtonpost.com/rss/rss_answer-sheet'
+    
+    
+    ]
+  
+  
+end
